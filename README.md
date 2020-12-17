d609162238f070d2a63e58babd560d3bc6eb6107
Raymond Wu <virus.warnning@gmail.com>
2020-12-17 18:04:38 +0800

    The container

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python-job-container
 An example to run python routine job in a container.
+
+```
+sudo docker-compose up
+```
---
+++ app/start.py
@@ -0,0 +1,38 @@
+import os
+import signal
+import subprocess
+import time
+
+close_requested = False
+
+def on_close():
+    close_requested = True
+
+def job():
+    print("I'm working.")
+
+def main():
+    # Validate process id.
+    if os.getpid() != 1:
+        print('This script must be run as root process.')
+        print('  pid: ', os.getpid())
+    signal.signal(signal.SIGTERM, on_close)
+
+    try:
+        mtime = os.path.getmtime('requirements.inst')
+    except:
+        mtime = 0
+
+    if mtime < os.path.getmtime('requirements.txt'):
+        # Install requirements.
+        subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+        subprocess.run(['touch', 'requirements.inst'])
+
+    import schedule
+    schedule.every(3).seconds.do(job)
+    while not close_requested:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
