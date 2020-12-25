b56f2f05fa28e80c40997656c5cbf9f1090dc58c
Raymond Wu <virus.warnning@gmail.com>
2020-12-25 15:05:34 +0800

    Handle unexpected exceptions.
    Mentions: #2

--- app/routine.py
+++ app/routine.py
@@ -12,10 +12,29 @@ def parallel(func):
         t.start()
     return wrapper
 
+def handle_unexpected(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as ex:
+            logger.error('Job "%s()" throws an unexpected exception.', func.__name__)
+            logger.error('  * Type: %s', type(ex).__name__)
+            logger.error('  * Message: %s', str(ex))
+    return wrapper
+
+# Run a job in parallel.
 @parallel
-def job():
+def good_job():
     logger.info("I'm working.")
 
+# Handle unexpected exceptions.
+# !!! @handle_unexpected must be placed under @parallel.
+@parallel
+@handle_unexpected
+def bad_job():
+    i = 1 / 0
+
+# Assign jobs here.
 def assign():
-    # Assign jobs here.
-    schedule.every(3).seconds.do(job)
+    schedule.every(3).seconds.do(good_job)
+    schedule.every(10).seconds.do(bad_job)
